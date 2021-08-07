f9236a09c3d04f64c12c32baaed286959ba021ea
Guilherme Cidade <guirevcidade1@gmail.com>
2021-08-07 17:27:33 -0300

    Possible moves of a piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.util.InputMismatchException;
+import java.util.Locale;
 import java.util.Scanner;
 
 import chess.ChessException;
@@ -12,6 +13,7 @@ public class Program {
 
 	public static void main(String[] args) {
 		
+		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
 		
