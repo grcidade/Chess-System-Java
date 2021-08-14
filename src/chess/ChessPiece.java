ef08dbec3d750deb67f250c6576ac47b39df8eeb
Guilherme Cidade <guirevcidade1@gmail.com>
2021-08-14 01:54:01 -0300

    Fixing error in moveCount

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -7,7 +7,7 @@ import boardgame.Position;
 public abstract class ChessPiece extends Piece {
 	
 	private Color color;
-	private Integer moveCount;
+	private int moveCount;
 
 	public ChessPiece(Board board, Color color) {
 		super(board);
